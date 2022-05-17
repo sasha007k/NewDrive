02d101f5937fe9194d4b8608596824b18a73c5bb
Oleksandr Kryklyvets <sashakryklyvets@gmail.com>
2022-05-18 01:26:17 +0300

    Added memory bar

---
+++ DAL/Migrations/20220517152640_DataMigration1705.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class DataMigration1705 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Folders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Files",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Folders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Files");
+        }
+    }
+}
